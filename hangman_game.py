e1aaab92c3c39e314ebe745acbfb1d1a7ab9c32f
AntoniCaGH <99190524+AntoniCaGH@users.noreply.github.com>
2023-03-06 21:27:28 +0000

    Added functionality to check if the user has successfully guessed all letters in a word correctly

--- hangman_game.py
+++ hangman_game.py
@@ -12,12 +12,21 @@ for _ in range(word_length):
 
 # print(display)
 
-guess = input("Choose a letter: ").lower()
+end_of_game = False
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-    if letter == guess:
-        display[position] = letter
+while not end_of_game:
 
-print(display)
+    guess = input("Choose a letter: ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
