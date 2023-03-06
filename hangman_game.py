a902190b2e1857ca9dfec84ba791041aaf6bf7a1
AntoniCaGH <99190524+AntoniCaGH@users.noreply.github.com>
2023-03-06 21:01:23 +0000

    Adding user input for letter guess and loop to check if letter appears in randomly chosen word

--- hangman_game.py
+++ hangman_game.py
@@ -3,3 +3,11 @@ import random
 word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
+
+guess = input("Choose a letter: ").lower()
+
+for char in chosen_word:
+    if char == guess:
+        print("Right")
+    else:
+        print("Wrong")
