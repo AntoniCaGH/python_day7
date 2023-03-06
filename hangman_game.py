87c78e665ac7d51439873666593ff8a40898d7be
AntoniCaGH <99190524+AntoniCaGH@users.noreply.github.com>
2023-03-06 21:11:31 +0000

    Adding functionality to replace blanks with user guess if user guess is correct

--- hangman_game.py
+++ hangman_game.py
@@ -4,10 +4,20 @@ word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
+display = []
+
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+
+# print(display)
+
 guess = input("Choose a letter: ").lower()
 
-for char in chosen_word:
-    if char == guess:
-        print("Right")
-    else:
-        print("Wrong")
+for position in range(word_length):
+    letter = chosen_word[position]
+    # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    if letter == guess:
+        display[position] = letter
+
+print(display)
